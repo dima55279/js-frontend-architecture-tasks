19e64d80e0b6940d1a9ad3a34d43451494054565
dima55279 <dima55279@yandex.ru>
2025-05-28 09:59:45 +0300

    Выполнено задание 1.

--- 1/src/application.js
+++ 1/src/application.js
@@ -1,3 +1,22 @@
 // BEGIN
-
+const calculator = () => {
+    const input = document.querySelector('[type="number"]');
+    input.focus();
+    const plus = document.querySelector('[type="submit"]');
+    plus.addEventListener('click', (e) => {
+        e.preventDefault();
+        result.textContent = (parseInt(result.textContent) + parseInt(input.value)).toString();
+        input.value = '';
+        input.focus();
+    })
+    const reset = document.querySelector('[type="button"]');
+    reset.addEventListener('click', (e) => {
+        e.preventDefault();
+        result.textContent = '0';
+        input.value = '';
+        input.focus();
+    })
+    const result = document.getElementById('result');
+}
+export default calculator;
 // END
