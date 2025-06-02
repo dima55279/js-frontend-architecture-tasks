75e33763935c7c9006f49f14594b6d36dd4520eb
dima55279 <dima55279@yandex.ru>
2025-06-02 22:22:10 +0300

    Выполнено задание 2.

--- 2/src/application.js
+++ 2/src/application.js
@@ -5,5 +5,33 @@ const routes = {
 };
 
 // BEGIN
+const toDo = async () => {
+    const ul = document.getElementById('tasks');
+    const button = document.querySelector('[type="submit"]');
+    const input = document.querySelector('[type="text"]');
 
+    const firstResponse = await axios.get(routes.tasksPath());
+    ul.innerHTML = '';
+    firstResponse.data.items.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item.name;
+        li.classList.add('list-group-item');
+        ul.appendChild(li);
+    });
+
+    button.addEventListener('click', async (e) => {
+        e.preventDefault();
+        await axios.post(routes.tasksPath(), { name: input.value });
+        input.value = '';
+        const secondResponse = await axios.get(routes.tasksPath());
+        ul.innerHTML = '';
+        secondResponse.data.items.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item.name;
+            li.classList.add('list-group-item');
+            ul.appendChild(li);
+        });
+    });
+};
+export default toDo;
 // END
