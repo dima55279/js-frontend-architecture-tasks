de8af897e1afb9d1177a874a98c574bb36a2d2c7
dima55279 <dima55279@yandex.ru>
2025-06-03 00:00:56 +0300

    Выполнено задание 4.

--- 4/src/application.js
+++ 4/src/application.js
@@ -1,3 +1,28 @@
 // BEGIN
-
+const companiesButtons = (companies) => {
+    const state = {
+        description: "",
+    }
+    const divContainer = document.querySelector(".container");
+    const divDescription = document.createElement("div");
+    divContainer.appendChild(divDescription);
+    companies.forEach(company => {
+        const button = document.createElement("button");
+        button.classList.add("btn");
+        button.classList.add("btn-primary");
+        button.appendChild(new Text(company.name));
+        divContainer.appendChild(button);
+        button.addEventListener("click", () => {
+            if (state.description === "" || state.description !== company.description) {
+                state.description = company.description;
+                divDescription.innerHTML = company.description;
+            }
+            else {
+                state.description = "";
+                divDescription.innerHTML = "";
+            }
+        })
+    })
+}
+export default companiesButtons;
 // END
